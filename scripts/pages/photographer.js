76a24a364ae69592e03f1dbbcb23cee6d82e05ef
Florent <florent.guyard@live.fr>
2023-12-03 20:29:37 +0100

    intégration page photographe

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,52 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+
+const selectPhotographerById = async (data)  => {
+
+    let photographerToDisplay = []
+    const photographerSelected = window.location.href.split('=').reverse()[0]
+    for (let i = 0; i< data.photographers.length; i++) {
+        if (data.photographers[i].id == photographerSelected) {
+            console.log(data.photographers[i])
+
+            photographerToDisplay.push(data.photographers[i])
+            console.log(photographerToDisplay)
+        } 
+    }
+
+    if (photographerToDisplay.length < 1) 
+    {
+        throw new Error('Aucun photographe ne correspond à votre recherche');
+        
+    } else {
+        return photographerToDisplay
+    }
+
+}
+
+const displayPhotographer = async (photographers) => {
+    const photographerProfil = document.querySelector('header');
+    console.log(photographers)
+    const photographerModel = photographerTemplate(photographers[0]);
+    const photographerCardDOM = photographerModel.getUserCardDOM();
+    photographerProfil.appendChild(photographerCardDOM);
+}
+
+async function getPhotographer() {
+
+    const response = await fetch('../../data/photographers.json')
+    if (!response.ok) {
+        throw new Error("Les informations n'ont pas pu être trouvées");
+    } else {
+        const data = await response.json();
+
+        const photographerToDisplay = await selectPhotographerById(data);
+        console.log(photographerToDisplay)
+        /*displayPhotographer(photographerToDisplay);*/
+        
+    }
+}
+
+
+
+getPhotographer();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,51 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
+function photographerTemplate(photographers) {
 
-    const picture = `assets/photographers/${portrait}`;
+    const portrait = photographers.portrait
+    const city = photographers.city;
+    const country = photographers.country;
+    const name = photographers.name;
+    const tagline = photographers.tagline;
+    const idPhotographer = photographers.id;
+    const price = photographers.price;
+    const picture = `../../Sample Photos/Photographers ID Photos/${portrait}`;
+
 
     function getUserCardDOM() {
+
+        let currentUrl = new URL('http://photographer.html')
+        currentUrl.searchParams.set('id', idPhotographer);
+
+
         const article = document.createElement( 'article' );
+        article.setAttribute("aria-label", `description de ${name}` )
+        const wrapperPicture = document.createElement('div');
+        wrapperPicture.classList.add('wrapperPicture');
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("alt", `photo de ${name}`);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const cityAndCountry = document.createElement('h3')
+        cityAndCountry.classList.add('cityAndCountry')
+        cityAndCountry.innerHTML = `${city}, ${country}`;
+        const tag = document.createElement('p');
+        tag.innerHTML = tagline;
+        const pricePhotographer = document.createElement('p')
+        pricePhotographer.id = 'photographer_section_price';
+        pricePhotographer.innerHTML = `${price}€/jour`
+        const link = document.createElement('a');
+        link.setAttribute('href', currentUrl.href );
+
+        article.appendChild(link);
+        link.appendChild(wrapperPicture)
+        wrapperPicture.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(cityAndCountry);
+        article.appendChild(tag);
+        article.appendChild(pricePhotographer);
+        
+        
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
